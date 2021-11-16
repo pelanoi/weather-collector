dcd82ed4a7a1db4c3bf3577feeeb0b2e717628c7
Surdu <neon-doubt@maskmail.net>
2021-11-16 23:39:08 +0200

    Fixed usbreset path

--- misc/reset.sh
+++ misc/reset.sh
@@ -2,4 +2,4 @@
 USBNAME=RTL2838
 LSUSB=$(lsusb | grep --ignore-case $USBNAME)
 DEVICE="/dev/bus/usb/"$(echo $LSUSB | cut --delimiter=' ' --fields='2')"/"$(echo $LSUSB | cut --delimiter=' ' --fields='4' | tr --delete ":")
-usbreset $DEVICE 2>&1 >/dev/null
+./misc/usbreset $DEVICE 2>&1 >/dev/null
